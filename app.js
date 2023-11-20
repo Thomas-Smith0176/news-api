6d3b3ea17caba33dd950ede43079de33bfa77012
Thomas-Smith0176 <thomas_smith@talktalk.net>
2023-11-20 13:56:46 +0000

    completes GET /api/articles/:article_id endpoint

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const { getTopics } = require('./controllers/topics-controllers');
+const { getArticle } = require('./controllers/articles-controllers');
 const app = express()
 
 app.get('/api/topics', getTopics);
 
+app.get('/api/articles/:article_id', getArticle);
+
 app.get('/api/*', (req, res) => {
     return res.status(404).send({msg: '404: Not found'})
-})
+});
 
 module.exports = app 
