e6bb1e29290830f2b292ba5dd142bc1c199e4d55
Thomas-Smith0176 <thomas_smith@talktalk.net>
2023-11-21 12:30:01 +0000

    completes POST /api/articles/article_id/comments endpoint

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const { getTopics } = require('./controllers/topics-controllers');
 const { getArticle } = require('./controllers/articles-controllers');
 const { getApis } = require('./controllers/api-controllers');
-const { getComments } = require('./controllers/comments-controllers');
+const { getComments, postComment } = require('./controllers/comments-controllers');
 const { getArticles } = require('./controllers/articles-controllers');
 const app = express()
+
+app.use(express.json())
 
 app.get('/api/topics', getTopics);
 
@@ -20,8 +22,11 @@ app.get('/api/*', (req, res) => {
     return res.status(404).send({msg: '404: Not found'})
 });
 
+app.post('/api/articles/:article_id/comments', postComment);
+
 app.use((err, req, res, next) => {
-    if (err.code === '22P02') {
+    const psqlCodes = ['22P02', '23503', '42703']
+    if (psqlCodes.includes(err.code)) {
         res.status(400).send({msg: 'Bad request'})
     };
     if(err.status) {
--- models/comments-models.js
+++ models/comments-models.js
@@ -1,4 +1,5 @@
 const db = require('../db/connection');
+const format = require('pg-format')
 
 exports.selectCommentsByArticleId = (articleId) => {
     return db.query(`
@@ -10,3 +11,17 @@ exports.selectCommentsByArticleId = (articleId) => {
         return response.rows
     });
 };
+
+exports.insertComment = (body, username, article_id,) => {
+    const created_at = new Date(Date.now())
+    const query = format(`
+    INSERT INTO comments
+    (body, author, article_id, votes, created_at)
+    VALUES
+    ( %L, %L, %s, 0, %L )
+    RETURNING *;`, body, username, article_id, created_at)
+    return db.query(query)
+    .then((response) => {
+        return response.rows[0]
+    })
+}
