29fa40f0804821435eefc0148eb0d0d3e4e16998
Thomas-Smith0176 <thomas_smith@talktalk.net>
2023-12-11 10:07:03 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const apiRouter = require('./routes/api-router');
 const { handleErrors } = require('./errors');
+const cors = require('cors');
 
 const app = express()
+
+app.use(cors());
 
 app.use(express.json())
 
